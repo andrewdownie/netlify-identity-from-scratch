f37be03a15e6f6f7a77c2c490b4c30a8aa481149
Drew Downie <drewdownie@Drews-MacBook-Air.local>
2019-10-23 21:46:08 -0400

    Adding unauthorized if the user isn't signed in, and a welcome message if they are signed in

--- src/lambda/authedBackendCall.js
+++ src/lambda/authedBackendCall.js
@@ -2,14 +2,24 @@ exports.handler = (event, context, callback) => {
     // Check if the user is signed in
     const claims = context.clientContext && context.clientContext.user;
 
-    const payload = {
-        context: context,
-        msg: 'claims is ' + claims
+    if(claims === undefined){
+        callback(null, {
+            statusCode: '401',
+            body: 'you need to sign in you friggen apple'
+        })
+    }
+    else{
+
+        const user = context.clientContext.user;
+        const userName = user.email.split('@')[0];
+
+        callback(null, {
+            statusCode: 200,
+            body: 'welcome to your profile' +  userName
+        })
+
     }
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(payload)
-    })
+
 
 }
